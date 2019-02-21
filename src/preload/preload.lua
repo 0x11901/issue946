4b67e6451f2720b2c0b91910941e1124b4d52c0c
0x11901 <wjk930726@live.com>
2019-02-21 11:58:50 +0800

    It's 2016; why are we using ColdFusion?!

--- src/preload/preload.lua
+++ src/preload/preload.lua
@@ -1,1 +1,1 @@
-
+require "error_code"
---
+++ src/service/area/init.lua
@@ -0,0 +1,12 @@
+local skynet = require "skynet"
+
+local module = {}
+
+function module.init()
+
+
+    -- 根据IP判断 启动相应的地图信息
+    skynet.error("area module init")
+end
+
+return module
